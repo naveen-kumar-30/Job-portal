620a48267e97c05004bf263f150ee027e1dfcf91
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-04-02 07:59:50 +0530

    Update vite.config.js

--- react-website/vite.config.js
+++ react-website/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api' : {
-        target: 'http://localhost:8000',
-        changeOrigin : true,
-        rewrite: (path) => path.replace(/^\/api/,''),
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:8000', // Use environment variable for live backend URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
-    }
-  }
+    },
+  },
 })
