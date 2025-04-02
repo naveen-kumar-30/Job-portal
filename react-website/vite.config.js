dbc8f04aa3ad0f65f75268b17db2e65089007422
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-04-02 10:06:50 +0530

    Update vite.config.js

--- react-website/vite.config.js
+++ react-website/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Allows Railway to bind to the correct address
-    port: process.env.PORT || 3000, // Use Railway-provided PORT
-  },
-  base: process.env.NODE_ENV === "production" ? "/" : "/", // Adjust if using a subdirectory
-});
+    port: 3000,
+    proxy: {
+      '/api' : {
+        target: 'http://localhost:8000',
+        changeOrigin : true,
+        rewrite: (path) => path.replace(/^\/api/,''),
+      },
+    }
+  }
+})
