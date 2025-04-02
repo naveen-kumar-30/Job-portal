fdf2312229de820e95046fb970aba1dfee21f2a2
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-04-02 09:23:17 +0530

    Update vite.config.js

--- react-website/vite.config.js
+++ react-website/vite.config.js
@@ -1,21 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures correct asset paths
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+      '/api' : {
+        target: 'http://localhost:8000',
+        changeOrigin : true,
+        rewrite: (path) => path.replace(/^\/api/,''),
       },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets', // Ensures JS & CSS files are placed inside /assets/
-  },
-});
+    }
+  }
+})
