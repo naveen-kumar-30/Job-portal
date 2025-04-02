fc6c7a52e08c7420ac5b5a69c7d26b0a813a575f
naveen-kumar-30 <naveenkumarp3052004@gmail.com>
2025-04-02 10:58:08 +0530

    update

--- react-website/src/App.jsx
+++ react-website/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Route,createBrowserRouter,createRoutesFromElements,RouterProvider,} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobPages from './pages/JobPages';
@@ -6,65 +6,57 @@ import NotFoundPage from './pages/NotFoundPage';
 import Jobpage, { jobLoader } from './pages/Jobpage';
 import AddJobPage from './pages/AddJobPage';
 import EditJobPage from './pages/EditJobPage';
-
-const API_BASE_URL = "https://job-portals-dguh.onrender.com";
+ 
 
 const App = () => {
-  // Add New Job
+  //Add New Job
   const addJob = async (newJob) => {
-    try {
-      const res = await fetch(`${API_BASE_URL}`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(newJob),
-      });
-      if (!res.ok) throw new Error("Failed to add job");
-      return res.json();
-    } catch (error) {
-      console.error("Error adding job:", error);
-    }
+    console.log(newJob);
+    const res = await fetch('/api/jobs',{
+      method: "POST",
+      headers : {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob),
+    })
+    return;
   };
 
-  // Delete Job
-  const deleteJob = async (id) => {
-    try {
-      const res = await fetch(`${API_BASE_URL}/${id}`, { method: "DELETE" });
-      if (!res.ok) throw new Error("Failed to delete job");
-      return res.json();
-    } catch (error) {
-      console.error("Error deleting job:", error);
-    }
-  };
+  //Delete Job
+  const deleteJob = async (id)=>{
+    const res = await fetch(`/api/jobs/${id}`,{
+      method: "DELETE",
+     
+    });
+    return;
+  } 
 
-  // Update Job
+  //update job
   const updateJob = async (job) => {
-    try {
-      const res = await fetch(`${API_BASE_URL}/${jobs.id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(job),
-      });
-      if (!res.ok) throw new Error("Failed to update job");
-      return res.json();
-    } catch (error) {
-      console.error("Error updating job:", error);
-    }
-  };
+    const res = await fetch(`/api/jobs/${job.id}`,{
+      method: 'PUT',
+      headers : {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job),
+    })
+    return;
+  }
+ 
+ const router = createBrowserRouter(
+   createRoutesFromElements(
+     <Route path='/' element={<MainLayout/>}>
+     <Route index element={<HomePage/>} />
+     <Route path='/jobs' element={<JobPages/>} />
+     <Route path='/add-job' element={<AddJobPage addJobSubmit = {addJob}/>}  />
+     <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader}/>
+     <Route path='/jobs/:id' element={<Jobpage deleteJob={deleteJob}/>} loader={jobLoader}/>
+     <Route path='*' element={<NotFoundPage/>} />
+     </Route>
+   )
+ );
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/jobs" element={<JobPages />} />
-        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path="/edit-job/:id" element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
-        <Route path="/jobs/:id" element={<Jobpage deleteJob={deleteJob} />} loader={jobLoader} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    )
-  );
-
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}/>;
 };
 
-export default App;
+export default App
