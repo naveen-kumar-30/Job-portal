45cdbcac0efb6961cf284bca4a1e71ac77c1ac4e
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-04-02 14:43:43 +0530

    Update vite.config.js

--- react-website/vite.config.js
+++ react-website/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target:'https://job-portals-dguh.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+      '/api' : {
+        target: 'http://localhost:8000',
+        changeOrigin : true,
+        rewrite: (path) => path.replace(/^\/api/,''),
       },
-    },
-  },
+    }
+  }
 })
